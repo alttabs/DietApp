35ccd23d58a4ce1e514b78f4dbe56070cc7dffb7
Bkaua <b.alves.amaral@gmail.com>
2019-04-25 00:15:29 -0300

    Codigo de amir, calculadora

--- app/src/main/java/com/example/dietaapp/MainActivity.kt
+++ app/src/main/java/com/example/dietaapp/MainActivity.kt
@@ -1,12 +1,45 @@
 package com.example.dietaapp
 
+
 import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
+import java.math.RoundingMode
+import java.text.DecimalFormat
+
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    companion object {
+
+        fun calculateBMI(weight: Double, height: Double): Pair<String, Int> {
+            val df = DecimalFormat("#.#")
+            df.roundingMode = RoundingMode.HALF_UP
+
+            val bmi = weight / (height * height)
+            return Pair("BMI\n" + df.format(bmi), bmi.toInt())
+        }
+
+        fun getCategoryIdentifier(bmiVal: Int): Int {
+            val categoryColorCode: Int
+
+            when (bmiVal) {
+                in 0..18 -> {
+                    categoryColorCode = R.color.blue
+                }
+                in 19..24 -> {
+                    categoryColorCode = R.color.green
+                }
+                in 25..29 -> {
+                    categoryColorCode = R.color.yellow
+                }
+                in 30..39 -> {
+                    categoryColorCode = R.color.orange
+                }
+                else -> {
+                    categoryColorCode = R.color.red
+                }
+            }
+            return categoryColorCode
+        }
     }
+
 }
