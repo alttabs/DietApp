04eade626cc7c13246d7057147260f635524f5d4
Thabata <engthabata@gmail.com>
2019-04-24 21:12:18 -0300

    forgot pass

--- app/src/main/java/com/example/dietaapp/LoginActivity.kt
+++ app/src/main/java/com/example/dietaapp/LoginActivity.kt
@@ -1,5 +1,7 @@
 package com.example.dietaapp
 
+import android.app.ProgressDialog
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Build
@@ -7,8 +9,29 @@ import android.view.Window
 import android.view.WindowManager
 import android.support.annotation.RequiresApi
 import android.support.v4.content.ContextCompat
+import android.text.TextUtils
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginActivity : AppCompatActivity() {
+
+    private val TAG = "LoginActivity"
+    //global variables
+    private var email: String? = null
+    private var password: String? = null
+    //UI elements
+    private var tvForgotPassword: TextView? = null
+    private var etEmail: EditText? = null
+    private var etPassword: EditText? = null
+    private var btnLogin: Button? = null
+    private var btnCreateAccount: Button? = null
+    private var mProgressBar: ProgressDialog? = null
+    //Firebase references
+    private var mAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -18,6 +41,8 @@ class LoginActivity : AppCompatActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             window.setStatusBarColorTo(R.color.colorPrimary)
         }
+
+        initialise ()
     }
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
@@ -25,6 +50,57 @@ class LoginActivity : AppCompatActivity() {
         this.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
         this.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         this.statusBarColor = ContextCompat.getColor(baseContext, color)
+    }
+
+    private fun initialise() {
+        tvForgotPassword = findViewById(R.id.tv_forgot_password) as TextView
+        etEmail = findViewById(R.id.et_email) as EditText
+        etPassword = findViewById(R.id.et_password) as EditText
+        btnLogin = findViewById(R.id.btn_login) as Button
+        btnCreateAccount = findViewById(R.id.btn_register_account) as Button
+        mProgressBar = ProgressDialog(this)
+        mAuth = FirebaseAuth.getInstance()
+        tvForgotPassword!!
+            .setOnClickListener { startActivity(Intent(this@LoginActivity,
+                ForgotPasswordActivity::class.java)) }
+        btnCreateAccount!!
+            .setOnClickListener { startActivity(
+                Intent(this@LoginActivity,
+                CreateAccountActivity::class.java)
+            ) }
+        btnLogin!!.setOnClickListener { loginUser() }
+    }
+
+
+    private fun loginUser() {
+        email = etEmail?.text.toString()
+        password = etPassword?.text.toString()
+        if (!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password)) {
+            mProgressBar!!.setMessage("Registering User...")
+            mProgressBar!!.show()
+            Log.d(TAG, "Logging in user.")
+            mAuth!!.signInWithEmailAndPassword(email!!, password!!)
+                .addOnCompleteListener(this) { task ->
+                    mProgressBar!!.hide()
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with signed-in user's information
+                        Log.d(TAG, "signInWithEmail:success")
+                        updateUI()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.e(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(this@LoginActivity, "Authentication failed.",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+        } else {
+            Toast.makeText(this, "Enter all details", Toast.LENGTH_SHORT).show()
+        }
+    }
+    private fun updateUI() {
+        val intent = Intent(this@LoginActivity, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        startActivity(intent)
     }
 }
 
