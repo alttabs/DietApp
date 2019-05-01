e9d32362770258efd3b8522e9f1c7ebd37a190d9
amird <amir.dantas@hotmail.com>
2019-05-01 14:24:25 -0300

    Exclusão de todos as variaveis infos Exclusão de multiplos textsviews Criação de Radiogroup e buttons para sexo Criação de Radiogroup e buttons para variavel fator de atividade E Atualização do calculo de TMB

--- app/src/main/java/com/example/dietaapp/Dieta1Activity.kt
+++ app/src/main/java/com/example/dietaapp/Dieta1Activity.kt
@@ -8,6 +8,23 @@ class Dieta1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dieta1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 
 
--- app/src/main/java/com/example/dietaapp/MainActivity.kt
+++ app/src/main/java/com/example/dietaapp/MainActivity.kt
@@ -4,48 +4,53 @@ package com.example.dietaapp
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
-import android.os.Handler
 import android.support.v7.app.AppCompatActivity
-import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.widget.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    //Declaração de variaveis
 
     var userHeightFirst: EditText? = null
     var userHeightSecond: EditText? = null
     var userWeight: EditText? = null
     var button: Button? = null
     var display: TextView? = null
+    var sexo: Boolean? = null
+    var TMB: Double? = null
+    var idade: Int? = null
+    var peso: Double? = null
+    var altura: Double? = null
+    var atividade: Int? = null
+    var natasha: DoubleArray = doubleArrayOf(1.0, 1.11, 1.25, 1.48)
+
 
     protected override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        userHeightFirst = findViewById(R.id.editText1)
-        userWeight = findViewById(R.id.editText3)
+        userHeightFirst = findViewById(R.id.etAltura)
+        userWeight = findViewById(R.id.etPeso)
         button = findViewById(R.id.button)
-        display = findViewById(R.id.textView2)
 
-        //start of the lame display part+
 
-        var info4: TextView = findViewById(R.id.textView4)
-        var info5: TextView = findViewById(R.id.textView5)
-        var info6: TextView = findViewById(R.id.textView6)
-        var info7: TextView = findViewById(R.id.textView7)
-        var info8: TextView = findViewById(R.id.textView8)
-        var info9: TextView = findViewById(R.id.textView9)
+        //Find view by id
 
-        info4.setTextColor(Color.BLACK)
-        info5.setTextColor(Color.BLACK)
-        info6.setTextColor(Color.BLACK)
-        info7.setTextColor(Color.BLACK)
-        info8.setTextColor(Color.BLACK)
-        info9.setTextColor(Color.BLACK)
-        display?.setTextColor(Color.BLACK)
-        //display?.setTextSize(30)
+        var etIdade = findViewById<EditText>(R.id.etIdade)
+        var etPeso = findViewById<EditText>(R.id.etPeso)
+        var etAltura = findViewById<EditText>(R.id.etAltura)
 
+
+        //
+
+        idade = etIdade.toString().toInt()
+        this.peso = etPeso.toString().toDoubleOrNull()
+        this.altura = etAltura.toString().toDoubleOrNull()
+
+
+
+        /*
         display?.text = "\tInserir Texto Aqui"
         info4.text = "Abaixo do peso     \t Menos que 18.5"
         info5.text = "Normal          \t\t\t\t\t 18.5 à 24.9"
@@ -54,10 +59,103 @@ class MainActivity : AppCompatActivity() {
         info8.text = "Obesidade classe ||  \t 35.0 à 39.9"
         info9.text = "Obesidade classe ||  \t Maior que 40"
 //end of the lame disply part
+*/
+
+        verificarRadioSexo()
+        verificarRadioAtividade()
+        //calcularTMB(altura!!, peso!!, idade!!, atividade!!)
 
 
         button?.setOnClickListener {startActivity(Intent(this@MainActivity, Dieta1Activity::class.java))
+
+            calcularTMB(altura!!, peso!!, idade!!, atividade!!)
+
             }
+        }
+
+
+        public fun verificarRadioSexo(){
+
+            rgSexo.setOnCheckedChangeListener { group, checkedId ->  {
+
+                if(checkedId == R.id.Mulher){
+
+                    this.sexo = true
+
+                } else if (checkedId == R.id.Homem){
+
+                    this.sexo = false
+
+                }
+
+
+            }}
+
+        }
+
+
+        public fun verificarRadioAtividade(){
+
+            rgAtividade.setOnCheckedChangeListener { group, checkedId ->  {
+
+                if(checkedId == R.id.rbSedentario){
+
+                    this.atividade = checkedId
+
+                } else if (checkedId == R.id.rbLeve){
+
+                    this.atividade = checkedId
+
+                } else if (checkedId == R.id.rbModerado){
+
+                    this.atividade = checkedId
+
+                } else if (checkedId == R.id.rbIntenso){
+
+                    this.atividade = checkedId
+
+                } else {
+
+                    //mensagemErro()
+
+                }
+
+
+            }}
+
+        }
+
+
+        public fun calcularTMB(altura: Double, peso: Double, idade: Int, atividade: Int){
+
+            val texto = findViewById<TextView>(R.id.textView3)
+
+            if(this.sexo == true){
+
+                TMB  = 66.5 -(idade * 9.5) + (natasha[atividade] * (15.9 * peso)) + (539.6 * altura )
+                texto.setText(TMB.toString())
+
+
+            } else if(this.sexo == false){
+
+                TMB  = 662 -(idade * 9.5) + (natasha[atividade] * (15.9 * peso)) + (539.6 * altura )
+                texto.setText(TMB.toString())
+
+            } else {
+
+                //mensagemErro()
+
+            }
+
+        }
+
+
+
+
+        public fun mensagemErro(){
+
+
+
         }
 
 
